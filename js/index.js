2b85c8e27b4b1ae42cf0103c7da40c19f86caae4
sirakisaias <sirakisaias@gmail.com>
2021-04-06 00:07:17 -0700

    finished project

--- js/index.js
+++ js/index.js
@@ -40,3 +40,54 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//nav section
+const navSection = document.querySelectorAll("a");
+//console.log(navSection);
+navSection.forEach(link => {
+      console.log(link.textContent);
+   })
+
+//header image 
+const headerImg = document.querySelector('#cta-img')   
+headerImg.setAttribute('src', 'img/header-img.png');
+// another way doing it is headerImg.src = "anylinkhere"
+
+//title
+const newTitle = document.querySelector('h1');
+newTitle.textContent = 'Dom Is Awesome';
+
+//button
+const newButton = document.querySelector('.cta button');
+newButton.textContent = 'Get Started';
+
+
+//middle image
+const midImage = document.querySelector('.middle-img');   
+midImage.src = 'img/mid-page-accent.jpg';
+
+//top-content
+const topFeature= document.querySelector('.top-content h4');
+topFeature.textContent = 'FEATURES';
+const topParagraph = document.querySelector('.text-content p');
+topParagraph.textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+// const topFeature2= document.querySelector('.text-content h4');
+// topFeature2.textContent = 'ABOUT';
+
+
+
+//bottom content
+const bottomContent= document.querySelector('.bottom-content h4');
+bottomContent.textContent = 'SERVICE';
+const bottomParagraph= document.querySelector('.bottom-content p');
+bottomParagraph.textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+
+
+//footer
+const bottomContact = document.querySelector('.contact h4');
+bottomContact.textContent = 'Contact'
+const address = document.querySelector('.contact p')
+address.textContent = '123 Way 456 Street Somewhere, USA';
+
+const newFooter = document.querySelector('footer');
+newFooter.textContent = 'Copyright Great Idea! 2018'
